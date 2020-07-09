a71e645b6a6d470bb313653906c97bb57a4dfb44
oded <oded@bit.dev>
2020-07-09 15:38:49 +0300

    explorer inital commit

---
+++ components/component-card/component-card.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './component-card.module.scss';
+
+export function ComponentCard() {
+    return (
+        <div></div>
+    )
+}
---
+++ components/component-grid/component-grid.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './component-grid.module.scss';
+
+export function ComponentGrid() {
+    return (
+        <div></div>
+    )
+}
